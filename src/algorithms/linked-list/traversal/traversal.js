1061136effbc9469711107bc36ca6d8f3f582c5a
HongJinHyeok <redmax45@naver.com>
2022-02-20 19:21:29 +0900

    init

--- src/algorithms/linked-list/traversal/traversal.js
+++
@@ -1,8 +0,0 @@
-export default function traversal(linkedList, callback) {
-  let currentNode = linkedList.head;
-
-  while (currentNode) {
-    callback(currentNode.value);
-    currentNode = currentNode.next;
-  }
-}

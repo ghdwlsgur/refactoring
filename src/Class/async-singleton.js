5a05a1f93d82915a433b7780fe9afc819792839d
HongJinHyeok <redmax45@naver.com>
2022-03-26 14:37:53 +0900

    async-singleton

---
+++ src/Class/async-singleton.js
@@ -0,0 +1,26 @@
+class AsyncSingleton {
+  static _instance;
+
+  constructor() {
+    if (AsyncSingleton._instance) return AsyncSingleton._instance;
+
+    return (async () => {
+      AsyncSingleton._instance = this;
+      return AsyncSingleton._instance;
+    })();
+  }
+
+  greet() {
+    console.log('hello world');
+  }
+}
+
+(async () => {
+  try {
+    const instance = await new AsyncSingleton();
+    console.log(instance);
+    instance.greet();
+  } catch (error) {
+    console.log(error);
+  }
+})();

823a02e20d4a51a8d95b24b8a4b042b3c6b8b62b
HongJinHyeok <redmax45@naver.com>
2022-04-03 18:12:38 +0900

    ♻️ 변수 추출하기 2단계

---
+++ src/Chapter6/3.ExtractVariable/phase-b.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// ! Sample Data ===================================
+const order = {
+  quantity: 8,
+  itemPrice: 1000,
+};
+
+// Example 1 ========================================
+function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  return (
+    basePrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  );
+}
+
+// ! Execute
+console.log(price(order));

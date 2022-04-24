2236c2d2593b66bbae96154e58c7fc8c9db78e4d
HongJinHyeok <redmax45@naver.com>
2022-04-24 14:33:56 +0900

    ✨ 단계 쪼개기 (배송비 계산 함수 추출)

---
+++ src/Chapter6/11.SplitPhase/phase-b.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+// 배송비 계산 함수 추출
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}

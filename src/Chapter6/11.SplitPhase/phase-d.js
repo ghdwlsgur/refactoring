cb9b62189b217146c1a1b6fc249cbf4b09700dce
HongJinHyeok <redmax45@naver.com>
2022-04-24 14:34:43 +0900

    ✨ 단계 쪼개기 (중간 데이터 구조를 활용하여 매개변수 전달 (basePrice)

---
+++ src/Chapter6/11.SplitPhase/phase-d.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const priceData = { basePrice }; // 중간 데이터 구조로 옮기는 작업 진행 (basePrice)
+  const price = applyShipping(priceData, shippingMethod, quantity, discount);
+  return price;
+}
+
+function applyShipping(priceData, shippingMethod, quantity, discount) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+
+  const shippingCost = quantity * shippingPerCase;
+  const price = priceData.basePrice - discount + shippingCost;
+  return price;
+}

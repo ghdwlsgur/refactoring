a7f95214b9b57173b4c2739b72e69932c0e102df
HongJinHyeok <redmax45@naver.com>
2022-05-08 17:22:29 +0900

    ✨ 문장을 호출한 곳으로 옮기기 (임시함수 생성)

---
+++ src/Chapter8/4.MoveStatementstoCallers/phase-b.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// 문장을 호출한 곳으로 옮기기
+
+function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+}
+
+function listRecentPhotos(outStream, photos) {
+  photos
+    .filter(p => p.date > recentDateCutoff())
+    .forEach(p => {
+      outStream.write('<div>\n');
+      emitPhotoData(outStream, p);
+      outStream.write('</div>\n');
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  zztmp(outStream, photo);
+  outStream.write(`<p>위치: ${photo.location}</p>`);
+}
+
+function zztmp(outStream, photo) {
+  outStream.write(`<p>제목: ${photo.title}</p>`);
+  outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>`);
+}

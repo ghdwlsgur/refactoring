68906ea170fe713d8d5163da202c9e12841e3333
HongJinHyeok <redmax45@naver.com>
2022-05-08 18:21:36 +0900

    ✨ 반복문 쪼개기 (부수효과 제거)

---
+++ src/Chapter8/7.SplitLoop/phase-b.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function totalSalaryOfYoungest() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  let totalSalary = 0;
+  for (const p of people) {
+    // if (p.age < youngest) youngest = p.age;
+    totalSalary += p.salary;
+  }
+
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
+    // totalSalary += p.salary;
+  }
+  return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
+}

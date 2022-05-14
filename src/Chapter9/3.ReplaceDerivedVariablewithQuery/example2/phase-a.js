4d95bffa584f29c27756e60a0a66d738c4a68783
HongJinHyeok <redmax45@naver.com>
2022-05-14 17:07:21 +0900

    ✨ 참조를 값으로 바꾸기 (소스가 둘 이상)

---
+++ src/Chapter9/3.ReplaceDerivedVariablewithQuery/example2/phase-a.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// 소스가 둘 이상일 때
+
+class ProductionPlan {
+  constructor(production) {
+    this._production = production;
+    this._adjustments = [];
+  }
+
+  get production() {
+    return this._production;
+  }
+
+  applyAdjustment(anAdjustment) {
+    this._adjustments.push(anAdjustment);
+    this._production += anAdjustment.amount;
+  }
+}

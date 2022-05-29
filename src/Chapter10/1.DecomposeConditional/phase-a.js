a145e24141c2ae5f1be6791520a80371d2792063
HongJinHyeok <redmax45@naver.com>
2022-05-29 22:01:03 +0900

    ✨ 조건문 분해하기

---
+++ src/Chapter10/1.DecomposeConditional/phase-a.js
@@ -0,0 +1,5 @@
+'use strict';
+
+if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+  charge = quantity * plan.summerRate;
+else charge = quantity * plan.regularRate + plan.regularServiceCharge;
